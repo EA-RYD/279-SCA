720d60a424b35180fafdef8a8a74f7ba8a55b646
Eric Arreola <32498849+EA-RYD@users.noreply.github.com>
2023-04-12 18:01:47 -0700

    Update SqlInjection.js

--- javascript-code-samples/SqlInjection.js
+++ javascript-code-samples/SqlInjection.js
@@ -3,14 +3,8 @@
 const db = require('mydb');
 
 const search = (query) => {
-  const sanitizedQuery = sanitizeInput(query);
-  const sql = `SELECT * FROM products WHERE name = '${sanitizedQuery}'`;
+  const sql = `SELECT * FROM products WHERE name = '${query}'`;
   return db.query(sql);
-};
-
-const sanitizeInput = (input) => {
-  const sanitizedInput = input.replace(/'/g, "''");
-  return sanitizedInput;
 };
 
 const userInput = req.query.name;
